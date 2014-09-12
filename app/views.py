6ca8e2434d06667f095044a8c539affd98e27acd
intspt <644596515@qq.com>
2014-09-12 18:42:14 +0800

    update

--- app/views.py
+++ app/views.py
@@ -5,12 +5,34 @@ from app import app
 import flask
 
 @app.route('/')
-def root():
+def home():
     return flask.render_template('index.html')
+
+@app.route('/register/')
+def register():
+    return flask.render_template('register.html')
 
 @app.route('/login/')
 def login():
     return flask.render_template('login.html')
+
+@app.route('/problemset/')
+def problemset():
+    return flask.render_template('base.html')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- run.py
+++ run.py
@@ -4,7 +4,7 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
 
 
 
